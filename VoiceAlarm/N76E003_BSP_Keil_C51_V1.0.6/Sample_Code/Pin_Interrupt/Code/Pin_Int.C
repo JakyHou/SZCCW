0618c9eaab75c6677ac851c196cf31ed43426af4
Jason <SZCCW>
2018-09-20 17:09:17 +0800

    use pin 0.7 as interrupt input, with 7M5 Ome pull up register

--- VoiceAlarm/N76E003_BSP_Keil_C51_V1.0.6/Sample_Code/Pin_Interrupt/Code/Pin_Int.C
+++ VoiceAlarm/N76E003_BSP_Keil_C51_V1.0.6/Sample_Code/Pin_Interrupt/Code/Pin_Int.C
@@ -25,10 +25,10 @@
 #define RLED 	P15
 #define P_SDA 	P00
 #define P_SCL 	P01
-
-int GFlag = 0;
-int RFlag = 0;
-
+#define INTP	P07
+static int GFlag = 0;
+static int RFlag = 0;
+static int repet = 1;
 
 /******************************************************************************
  * FUNCTION_PURPOSE: I/O Pin interrupt Service Routine
@@ -57,8 +57,28 @@ void SendCMD(int cmd)
 	P_SDA = 1;
 }
 
-void PinInit()
+void PinInterrupt_ISR (void) interrupt 7 // Pin interrupt server. level 7
 {
+	
+		PIF = 0x00;        	        // clear interrupt flag
+		repet = 1;
+		GFlag = 6;
+		RFlag = 30;
+		// Timer1_Delay10ms(10);
+
+}
+
+
+/******************************************************************************
+The main C function.  Program execution starts
+here after stack initialization.
+******************************************************************************/
+void main (void) 
+{
+
+
+
+
 	//Green LED output
 	P10_PushPull_Mode;
 	GLED = 0;
@@ -71,108 +91,78 @@ void PinInit()
 	P_SDA = 1;
 	P_SCL = 1;
 	 
-  	P17_Input_Mode; // Set p1.7 input mode
-	set_P1S_7;			// set p1.7 voltage level mode
-	Enable_INT_Port1; 		// Set interrupt IO port 
+    P07_Input_Mode; // Set p1.7 input mode
+	set_P0S_7;			// set p1.7 voltage level mode
+	Enable_INT_Port0; 		// Set interrupt IO port 
 	Enable_BIT7_RasingEdge_Trig; // Set pin7 Triger mode
-}
 
-void PinInterrupt_ISR (void) interrupt 7 // Pin interrupt server. level 7
-{
-	
-	PIF = 0x00;        	        // clear interrupt flag
-
-}
-
-
-/******************************************************************************
-The main C function.  Program execution starts
-here after stack initialization.
-******************************************************************************/
-void main (void) 
-{
-
-	PinInit();
-
-	Timer0_Delay1ms(200);
-    set_EPI;							// Enable pin interrupt	
+    set_EPI;							// Enable pin interrupt
     set_EA;								// global interrupt enable bit
-	// PIF = 0x00;
-	clr_BODEN;							// disable brown out detection
-		
-	// PIF = 0x80;
-
+	clr_BODEN;
+//		set_PD;
+	Timer0_Delay1ms(200);
     while (1)
 	{
+		RFlag = 30;
+		GFlag = 6;
 
-
-		
-
-		if(P17 != 0)
+		// PowerDown mode
+		while(GFlag > 0)
 		{
-			//		int count = 3;
-			Timer0_Delay1ms(5);
-			if(P17 != 0)
+			if(INTP != 0)
 			{
-				// SendCMD(0xFE);				// stop voice alarm
-				// Timer0_Delay1ms(10);
-				//SendCMD(0x4);				
-				// Timer0_Delay1ms(10);
-				// SendCMD(0xF2);
-				GLED = 0;					// turn off green led
-				RLED = 0;
-				// GFlag = 0;
 				RFlag = 30;
-				SendCMD(0x4);	
-				Timer0_Delay1ms(10);
-				SendCMD(0xF2);
-				while(RFlag > 0)
+				break;
+			}
+			GLED = 1;
+			Timer0_Delay1ms(200);
+			GLED = 0;
+			Timer0_Delay1ms(300);
+			GFlag--;
+		}
+		if(GFlag == 0) repet = 0;
+		while(repet>0)
+		{
+			if(INTP != 0)
+			{
+				Timer0_Delay1ms(1);
+				if(INTP != 0)
 				{
-					if(P17 != 0)
+					// Timer0_Delay1ms(20);
+					SendCMD(0x4);
+					Timer0_Delay1ms(5);
+					SendCMD(0xF2);
+					while(RFlag > 0)
 					{
+						if(INTP == 0)
+						{
+							SendCMD(0xFE);
+							GFlag = 6;
+							break;
+						}
 						RLED = 1;
 						Timer0_Delay1ms(500);
 						RLED = 0;
 						Timer0_Delay1ms(500);
 						RFlag--;
 					}
-					else
+					SendCMD(0xFE);
+					if(RFlag != 0)
+					while(GFlag > 0)
 					{
-						SendCMD(0xFE);
-						GFlag = 6;
-						while(GFlag)
-						{
-							GLED = 1;
-							Timer0_Delay1ms(200);
-							GLED = 0;
-							Timer0_Delay1ms(300);
-							GFlag--;
-						}
-						RFlag = 0;
-					}
-				}
-			}
-					
-		}
-		else
-			{
-				Timer0_Delay1ms(5);
-				if(P17 == 0)
-				{
-					GFlag = 6;
-					while(GFlag)
-					{
+						if(INTP != 0) 
+						{repet = 1; break;}
 						GLED = 1;
 						Timer0_Delay1ms(200);
 						GLED = 0;
 						Timer0_Delay1ms(300);
 						GFlag--;
 					}
+					else repet = 0;
+					// break;
 				}
-			}	
-				set_PD;
+			}
 		}
-	
-
-	
+		set_PD;
+	}
 }
